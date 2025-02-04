71a68ebbd8829865e53bb932ece557759adc147e
16319 <hjaltekappelcordt@gmail.com>
2025-02-04 13:40:22 +0100

    new

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,4 +1,10 @@
-
+let player = {
+    x: 0,
+    y: 0,
+    speed: 5,
+    size: 30,
+    
+}
 
 function setup()
 {
@@ -9,6 +15,23 @@ function setup()
 
 function draw()
 {
+    if (keyIsDown(LEFT_ARROW)) {
+        player.x -= player.speed
+    }
+    
+    if (keyIsDown(RIGHT_ARROW)) {
+       player.x += player.speed
+    }
+    
+    if (keyIsDown(UP_ARROW)) {
+        player.y -= player.speed
+    }
+    
+    if (keyIsDown(DOWN_ARROW)) {
+        player.y += player.speed
+    }
 
+    fill(100,100,100)
+    circle(player.x, player.y, player.size)
 
 }
